b1b94d9ce7be62b50be6bda06fd2a4c73c2810fa
Roey Zalta <roey.zalta@gmail.com>
2024-11-08 14:00:23 +0200

    created a pipeline as a whole string

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,15 @@
 import logging
 import apache_beam as beam
+import argparse
 
-def run():
-    with beam.Pipeline() as pipeline:
-        (
-            pipeline
-            | 'Create' >> beam.Create([1, 2, 3, 4, 5])
-            | 'Print' >> beam.ParDo(lambda x: logging.warning(x))
-        )
+def run(argv=None):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--input')
+        parser.add_argument('--output')
+        args, beam_args = parser.parse_known_args(argv)
+        with beam.Pipeline(argv = beam_args) as p:
+            lines = p | "ReadFile" >> beam.io.ReadFromText(args.input, skip_header_lines=1)
+            lines | beam.Map(print)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.WARNING)
